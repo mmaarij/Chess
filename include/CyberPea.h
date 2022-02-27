9655706250d11468863b8230499f7e56d91d71ca
mmaarij <mmaarijn@gmail.com>
2022-02-27 20:12:16 +0500

    Added 2nd Level MiniMax Algo (Board Not Working)

--- include/CyberPea.h
+++ include/CyberPea.h
@@ -14,6 +14,13 @@ using namespace std;
 #define QUEEN 90
 #define KING 900
 
+class Data
+{
+    public:
+        int score;
+        int index;
+};
+
 class CyberPea : public chessPlayer {
     public:
         // Constructor
@@ -30,15 +37,23 @@ class CyberPea : public chessPlayer {
                 return;
             }
 
-            int SelectedMove = evalScore(state, TotalMoves);
+            Data bestMove;
+            bestMove = minimax(state, 1);
+            int SelectedMove = bestMove.index;
+            if (bestMove.score == 0)
+            {
+                srand(time(0));
+                SelectedMove = rand() % TotalMoves;
+            }
+            
 
             state->Actions.getAction(SelectedMove, Move);
 
             return;
         }
 
-        // Calculate Score
-        int evalScore(gameState* state, int TotalMoves)
+        // Calculate Score 
+        int evalScoreFirstLvl(gameState* state, int TotalMoves)
         {
             int maxIndex = 0;
             int maxScore = INT_MIN;
@@ -111,6 +126,147 @@ class CyberPea : public chessPlayer {
             return maxIndex;
         }
 
+
+        Data minimax(gameState* state, int depth)
+        {
+            //we want to compute all possible moves here.
+            int TotalMoves = state->Actions.getActionCount(); // this == computepossibleactions
+            actionList tempList;
+            action tempAction;
+            
+            
+
+
+            if (depth == 0 || TotalMoves == 0) // base case
+            {
+                Data leaf;
+                leaf.score = calcScore(state);
+                return leaf;
+            }
+            else
+            {
+                //backup action list
+                for (int i = 0; i < TotalMoves; i++)
+                {
+                    state->Actions.getAction(i, &tempAction);
+                    tempList.addAction(tempAction);
+                }
+            }
+
+            if (state->getPlayer() == White)
+            {
+                Data maxEval;
+                maxEval.score = INT_MIN;
+
+                for (int i = 0; i < TotalMoves; i++)
+                {
+                    state->Actions.getAction(i, &tempAction);
+
+                    // make look-ahead move on board and change player
+                    state->applyMove(tempAction);
+
+                    Data eval = minimax(state, depth - 1);
+
+                    // undo look-ahead move on board
+                    state->Board.board[tempAction.fromRow][tempAction.fromCol] = state->Board.board[tempAction.toRow][tempAction.toCol];
+                    state->Board.board[tempAction.toRow][tempAction.toCol] = 0;
+                    state->setPlayer(White);
+                    state->Actions.resetActions();
+                    for (int j = 0; j < TotalMoves; j++)
+                    {
+                        tempList.getAction(j, &tempAction);
+                        state->Actions.addAction(tempAction);
+                    }
+
+                    if (eval.score > maxEval.score)
+                    {
+                        maxEval.score = eval.score;
+                        maxEval.index = i;
+                    }
+                }
+                return maxEval;
+            }
+
+            else
+            {
+                Data minEval;
+                minEval.score = INT_MAX;
+
+                for (int i = 0; i < TotalMoves; i++)
+                {
+                    state->Actions.getAction(i, &tempAction);
+
+                    // make look-ahead move on board and change player
+                    state->applyMove(tempAction);
+
+                    Data eval = minimax(state, depth - 1);
+
+                    // undo look-ahead move on board
+                    state->Board.board[tempAction.fromRow][tempAction.fromCol] = state->Board.board[tempAction.toRow][tempAction.toCol];
+                    state->Board.board[tempAction.toRow][tempAction.toCol] = 0;
+                    state->setPlayer(Black);
+                    state->Actions.resetActions();
+                    for (int j = 0; j < TotalMoves; j++)
+                    {
+                        tempList.getAction(j, &tempAction);
+                        state->Actions.addAction(tempAction);
+                    }
+
+                    if (eval.score < minEval.score)
+                    {
+                        minEval.score = eval.score;
+                        minEval.index = i;
+                    }
+                }
+                return minEval;
+            }
+            
+        }
+
+
+        int calcScore(gameState* state)
+        {
+            int tempScore = 0;
+
+            // evaluate move score
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (state->Board.board[x][y] == 1)
+                        tempScore += PAWN;
+                    else if (state->Board.board[x][y] == -1)
+                        tempScore -= PAWN;
+
+                    else if (state->Board.board[x][y] == 4)
+                        tempScore += ROOK;
+                    else if (state->Board.board[x][y] == -4)
+                        tempScore -= ROOK;
+
+                    else if (state->Board.board[x][y] == 2)
+                        tempScore += KNIGHT;
+                    else if (state->Board.board[x][y] == -2)
+                        tempScore -= KNIGHT;
+
+                    else if (state->Board.board[x][y] == 3)
+                        tempScore += BISHOP;
+                    else if (state->Board.board[x][y] == -3)
+                        tempScore -= BISHOP;
+
+                    else if (state->Board.board[x][y] == 5)
+                        tempScore += QUEEN;
+                    else if (state->Board.board[x][y] == -5)
+                        tempScore -= QUEEN;
+
+                    else if (state->Board.board[x][y] == 6)
+                        tempScore += KING;
+                    else if (state->Board.board[x][y] == -6)
+                        tempScore -= KING;
+                }
+            }
+
+            return tempScore;
+        }
 };
 
 
